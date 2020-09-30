b5f9edecfb32b9467063880e0219c76d917938b2
beni2412 <hardaman97@gmail.com>
2020-09-30 15:37:35 +0530

    [Hardaman] Added: Welcome Message

---
+++ src/main/java/UserRegistration/UserValidation.java
@@ -0,0 +1,15 @@
+package UserRegistration;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class UserValidation {
+
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to User Registration");
+		
+			
+	}
+}
