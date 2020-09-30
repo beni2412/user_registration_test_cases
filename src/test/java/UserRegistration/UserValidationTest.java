5d63fcd71b346665d1fb12084a057051538e8e0a
beni2412 <hardaman97@gmail.com>
2020-09-30 21:11:12 +0530

    [Hardaman] Added: Custom exception class and try and catch blocks

--- src/test/java/UserRegistration/UserValidationTest.java
+++ src/test/java/UserRegistration/UserValidationTest.java
@@ -1,6 +1,7 @@
 package UserRegistration;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.Assert;
 
 public class UserValidationTest {
@@ -8,62 +9,134 @@ public class UserValidationTest {
 		 @Test
 		    public void givenFirstName_Proper_ReturnTrue() {
 		        UserValidation user = new UserValidation();
-		        boolean result = user.firstNameValidation("Hardaman");
-		        Assert.assertTrue(true);
+		        boolean result =false;
+		        try {
+		        	ExpectedException exceptionRule = ExpectedException.none();
+					exceptionRule.expect(UserValidationException.class);
+					result = user.firstNameValidation("Hardaman");
+				} catch (UserValidationException e) {
+					e.printStackTrace();
+				}
+		        Assert.assertTrue(result);
 		    }
 		 @Test
 		    public void givenFirstName_Improper_ReturnFalse() {
 			 UserValidation user = new UserValidation();
-		        boolean result = user.firstNameValidation("hardaman");
-		        Assert.assertFalse(false);
+			 boolean result =false;
+		        try {
+		        	ExpectedException exceptionRule = ExpectedException.none();
+					exceptionRule.expect(UserValidationException.class);
+					result = user.firstNameValidation("hardaman");
+				} catch (UserValidationException e) {
+					e.printStackTrace();
+				}
+		        Assert.assertFalse(result);
 		    }
 		 @Test
 		    public void givenLastName_Proper_ReturnTrue() {
 			 UserValidation user = new UserValidation();
-		        boolean result = user.lastNameValidation("Benipal");
-		        Assert.assertTrue(true);
+			 boolean result =false;
+		        try {
+		        	ExpectedException exceptionRule = ExpectedException.none();
+					exceptionRule.expect(UserValidationException.class);
+					result = user.lastNameValidation("Benipal");
+				} catch (UserValidationException e) {
+					e.printStackTrace();
+				}
+		        Assert.assertTrue(result);
 		    }
 		 @Test
 		    public void givenLastName_Improper_ReturnFalse() {
 			 UserValidation user = new UserValidation();
-		        boolean result = user.lastNameValidation("benipal");
-		        Assert.assertFalse(false);
+			 boolean result =false;
+		        try {
+		        	ExpectedException exceptionRule = ExpectedException.none();
+					exceptionRule.expect(UserValidationException.class);
+					result = user.lastNameValidation("benipal");
+				} catch (UserValidationException e) {
+					e.printStackTrace();
+				}
+		        Assert.assertFalse(result);
 		    }
 		 @Test
 		    public void givenEmail_Proper_ReturnTruee() {
 			 UserValidation user = new UserValidation();
-		        boolean result = user.emailIDValidation("abc@gmail.com");
-		        Assert.assertTrue(true);
+			 boolean result =false;
+		        try {
+		        	ExpectedException exceptionRule = ExpectedException.none();
+					exceptionRule.expect(UserValidationException.class);
+					result = user.emailIDValidation("abc@gmail.com");
+				} catch (UserValidationException e) {
+					e.printStackTrace();
+				}
+		        Assert.assertTrue(result);
 		    }
 		 @Test
 		    public void givenEmail_Improper_ReturnFalse() {
 			 UserValidation user = new UserValidation();
-		        boolean result = user.emailIDValidation("abcdef");
-		        Assert.assertFalse(false);
+			 boolean result =false;
+		        try {
+		        	ExpectedException exceptionRule = ExpectedException.none();
+					exceptionRule.expect(UserValidationException.class);
+					result = user.emailIDValidation("abcdef");
+				} catch (UserValidationException e) {
+					e.printStackTrace();
+				}
+		        Assert.assertFalse(result);
 		    }
 		 @Test
 		    public void givenPassword_Proper_ReturnTrue() {
 			 UserValidation user = new UserValidation();
-		        boolean result = user.passwordValidation("Abcde24@");
-		        Assert.assertTrue(true);
+			 boolean result =false;
+			 
+		        try {
+		        	ExpectedException exceptionRule = ExpectedException.none();
+					exceptionRule.expect(UserValidationException.class);
+					result = user.passwordValidation("Abcde24@");
+				} catch (UserValidationException e) {
+					e.printStackTrace();
+				}
+		        Assert.assertTrue(result);
 		    }
 		 @Test
 		    public void givenPassword_Improper_ReturnFalse() {
 			 UserValidation user = new UserValidation();
-		        boolean result = user.passwordValidation("helloworld");
-		        Assert.assertFalse(false);
+			 boolean result =false;
+			 
+		        try {
+		        	ExpectedException exceptionRule = ExpectedException.none();
+					exceptionRule.expect(UserValidationException.class);
+					result = user.passwordValidation("helloworld");
+				} catch (UserValidationException e) {
+					e.printStackTrace();
+				}
+		        Assert.assertFalse(result);
 		    }
 		 @Test
 		    public void givenMobileNo_Proper_ReturnTrue() {
 			 UserValidation user = new UserValidation();
-		        boolean result = user.phoneNumberValidation("91 1234567895");
-		        Assert.assertTrue(true);
+			 boolean result =false;
+		        try {
+		        	ExpectedException exceptionRule = ExpectedException.none();
+					exceptionRule.expect(UserValidationException.class);
+					result = user.phoneNumberValidation("91 1234567895");
+				} catch (UserValidationException e) {
+					e.printStackTrace();
+				}
+		        Assert.assertTrue(result);
 		    }
 		 @Test
 		    public void givenMobileNo_Improper_ReturnFalse() {
 			 UserValidation user = new UserValidation();
-		        boolean result = user.phoneNumberValidation("12345");
-		        Assert.assertFalse(false);
+			 boolean result =false;
+		        try {
+		        	ExpectedException exceptionRule = ExpectedException.none();
+					exceptionRule.expect(UserValidationException.class);
+					result = user.phoneNumberValidation("12345");
+				} catch (UserValidationException e) {
+					e.printStackTrace();
+				}
+		        Assert.assertFalse(result);
 		    }
 	
 }
