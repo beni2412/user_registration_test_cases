53fd156af26ea83dbb3f8f381f7555fa479b9a5c
beni2412 <hardaman97@gmail.com>
2020-09-30 15:56:06 +0530

    [Hardaman] Added: Validation of first name

--- src/main/java/UserRegistration/UserValidation.java
+++ src/main/java/UserRegistration/UserValidation.java
@@ -4,12 +4,32 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserValidation {
 
-
+	public static boolean firstNameValidation(String fname) {
+		Pattern pattern =Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+		Matcher matcher = pattern.matcher(fname);
+		boolean matchfound = matcher.find();
+		if(matchfound) 
+			return true;
+		else 
+			return false;
+		
+	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to User Registration");
-		
+		for(int i=0;;) {
+			boolean found = false;
+			System.out.println("Enter first name (Start with capital):");
+			String firstName = sc.nextLine();
+			found = firstNameValidation(firstName);
+			if(found) {
+				System.out.println("Correct input");
+				break;
+			}
+			else
+				System.out.println("Invalid name (Start with capital and should have min 3 charcaters");
+		}
 			
 	}
 }
